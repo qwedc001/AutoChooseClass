1a96927c1370a7cf0426ce12e9b044f4d04ec8b1
EricGuo <2364319479@qq.com>
2024-01-14 20:56:59 +0800

    feat.增加了体育课抢课判定，初步修改程序

--- configure.py
+++
@@ -1,4 +0,0 @@
-class Configure:
-    def __init__(self):
-        self.username = ''  # 请在此处输入你的用户名
-        self.password = ''  # 请在此处输入你的密码
--- spidertest.py
+++ spidertest.py
@@ -1,89 +1,66 @@
 import time
-from configure import Configure
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 # 初始化,可以进行相关的配置
 def init(url):
-    driver = webdriver.Chrome()
+    print("[debug]执行初始化")
+    chrome_options = Options()
+    chrome_options.add_experimental_option("debuggerAddress","127.0.0.1:9887")
+    print("[debug]已设置debugger参数")
+    driver = webdriver.Chrome(options=chrome_options)
+    print("[debug]已获取当前chrome程序")
     driver.get(url)
     wait = WebDriverWait(driver, 10)
     return driver, wait
 
-
-# 此函数专门用于教务系统的登录与页面跳转的处理
-def auto_login(driver, wait):
-    cfg = Configure()
-    wait.until(EC.presence_of_element_located((By.XPATH,
-                                               '/html/body/app-root/app-right-root/rg-page-container/div/div[2]/div/div[2]/div[2]/div/app-login-auth-panel/div/div[1]/app-login-normal/div/div[2]/form/div[1]/nz-input-group/input'))).send_keys(
-        cfg.username)
-
-    wait.until(EC.presence_of_element_located((By.XPATH,
-                                               '/html/body/app-root/app-right-root/rg-page-container/div/div[2]/div/div[2]/div[2]/div/app-login-auth-panel/div/div[1]/app-login-normal/div/div[2]/form/div[2]/nz-input-group/input'))).send_keys(
-        cfg.password)
-
-    wait.until(EC.presence_of_element_located((By.XPATH,
-                                               '/html/body/app-root/app-right-root/rg-page-container/div/div[2]/div/div[2]/div[2]/div/app-login-auth-panel/div/div[1]/app-login-normal/div/div[2]/form/div[6]/div/button'))).click()
-
-    # 唯一可能发生问题的地方
-    flag = False
-    while not flag:
-        try:
-            driver.refresh()
-            wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[8]/div[1]'))).click()
-            flag = True
-        except Exception as e:
-            pass
-
-    wait.until(EC.presence_of_element_located((By.XPATH,
-                                               '/html/body/div[1]/section/section/main/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div[13]/span'))).click()
-
-    # 页面跳转1
+def choose(driver:webdriver.Chrome, wait:WebDriverWait):
+    start_time = time.time()
+    # 点击查询
+    wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[1]/div/ul/li[2]/a'))).click() # 这里对应“自主选课”按钮在我的应用的位置
     all_handles = driver.window_handles
     new_window_handle = all_handles[1]
     driver.switch_to.window(new_window_handle)
-
-    wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[1]/div/ul/li[6]/a'))).click()
-
-    # 页面跳转2
-    all_handles = driver.window_handles
-    new_window_handle = all_handles[2]
-    driver.switch_to.window(new_window_handle)
-
-
-# 自主选课设计——根据你自己的需求进行相应的改写
-def choose(driver, wait):
-    # 点击查询
     wait.until(EC.presence_of_element_located(
         (By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div[1]/div/div/div/div/span/button[1]'))).click()
-
-    # 定位选课列表
-    cls_list = driver.find_elements(By.XPATH, '//*[@class="body_tr"]')
-    # 做一个小小的记录
-    total = len(cls_list)
+    # 定位体育课列表
+    PE = True
+    if PE:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@class="panel panel-info"]/ul/li/a[@id="tab_kklx_06"]'))).click()
+    # 定位所有选课列表
+    classNames = [
+        '大学体育（1）(2023-2024-2)-B151118-5', # 户外运动
+        '大学体育（1）(2023-2024-2)-B151107-7'  # 排球
+    ]
+    time.sleep(3)
     succ = 0
     fail = 0
-    # 主动降速等待页面加载
-    time.sleep(1.8)
-    for cls in cls_list:
+    total = len(classNames)
+    for className in classNames:
         try:
-            cls.find_element(By.XPATH, './/*[@class="an"]').click()
+            curClass = driver.find_element(By.XPATH, '//*[@class="body_tr"]/td[@class="jxbmc" and text()="'+className+'"]')
+            print("找到课程"+className)
+            curClass.find_element(By.XPATH, './/*[@class="an"]').click()
             succ += 1
+            break # 体育课只允许选择一门，选择成功后就退出
         except Exception as e:
-            print('无法定位元素')
+            print(f'[Error]无法定位课程{className}/该课程的按钮元素')
             fail += 1
-    return total, succ, fail
+    end_time = time.time()
+    return total, succ, fail,end_time - start_time
 
 
 if __name__ == '__main__':
-    s1 = time.time()
-    url = 'https://sso.qlu.edu.cn/login'
+    url = 'https://jw.qlu.edu.cn'
     driver, wait = init(url)
-    auto_login(driver, wait)
-    total, succ, fail = choose(driver, wait)
-    s2 = time.time()
-    print(f'已全部完成,总的选课数{total},成功选课数{succ},失败选课数{fail}')
-    print('总用时:', s2 - s1, '秒')
+    # auto_login(driver, wait)
+    print("已完成登入，请在将要抢课的时候按下回车")
+    input()
+    total, succ, fail, usedTime = choose(driver, wait)
+    print(f'已全部完成,总的选课数{total},成功选课数{succ},失败选课数{fail},用时{round(usedTime,2)}秒')
